684940c7ee1500f705b11c66cda2b66bd70f8605
kkout <koutsompinaskk@gmail.com>
2023-07-03 19:22:54 +0300

    first commit

---
+++ proj_tedi-1/src/main/java/com/project/tedi/ProjTedi1Application.java
@@ -0,0 +1,13 @@
+package com.project.tedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjTedi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjTedi1Application.class, args);
+	}
+
+}
